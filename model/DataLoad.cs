cc38d6e70dbfaebb7cf0cf0a23d57eebae0cac50
Tiago Oliveira <92068949+Titopollice@users.noreply.github.com>
2024-05-05 20:42:05 -0300

    ajuste geral do docker

--- model/DataLoad.cs
+++ model/DataLoad.cs
@@ -6,20 +6,23 @@ using System.IO;
 
 public class DataLoad : IDataLoad
 {
-  public List<Data> Search()
-  {
-    return Load<Data>("https://github.com/RhuanRP/Insurence_API/blob/main/data/Car_Insurance_Claim.csv");
-  }
-  public List<T> Load<T>(string local)
-  {
-    local = local + ".csv";
-    if (!File.Exists(local))
-      throw new ArgumentException(local);
+    private string dataDirectory = @"C:\Users\TIAGO\Documents\Dev\CScoreAPI\CreditScoreAPI\data\";
 
-    using (var reader = new StreamReader(local))
-    using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+    public List<Data> Search()
     {
-      return csv.GetRecords<T>().ToList();
+        string filePath = Path.Combine(dataDirectory, "Car_Insurance_Claim.csv");
+        return Load<Data>(filePath);
     }
-  }
+
+    public List<T> Load<T>(string local)
+    {
+        if (!File.Exists(local))
+            throw new ArgumentException("O arquivo não foi encontrado: " + local);
+
+        using (var reader = new StreamReader(local))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            return csv.GetRecords<T>().ToList();
+        }
+    }
 }
